919af9a470d4e702461f62607ccf31dcca1dbc5c
Johnathon Sykes <hireme@mrjsykes.com>
2017-09-17 12:37:10 -0700

    Checkpoint: Video 10

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,6 +1,7 @@
 // a reducer takes two arguments, an action and a copy of current state
 
 var posts = (state = [], action)=> {
+  console.log("the post will change")
   console.log(state, action)
   return state
 }
